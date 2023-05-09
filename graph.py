21662d15d1c46d5dc26ff4bd99cd1f492c38c9c5
Veronica Galanter <vgalanter2025@kehillahstudent.org>
2023-05-09 09:10:11 -0700

    Percent of Each Language Speaker Graph

--- graph.py
+++ graph.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 
+#gets the website and table information
 tables = pd.read_html(
     "https://en.wikipedia.org/wiki/List_of_languages_by_total_number_of_speakers",
     match = "Most-spoken languages, CIA, 2022"
@@ -10,10 +11,17 @@ tables = pd.read_html(
 )
 
 table = tables[0]
+#sets up table
 df = table[["Language", "Percentage of world population (2022)"]]
+#prints out the percent info
 print (df.info())
+#makes percent recognized as a numerical value for printing
 df["Percent"]=df["Percentage of world population (2022)"].str.extract('(\d+)').astype(int)
+#prints out percent as numerical value
 print (df.info())
-df.plot(x = "Language", y = "Percent")
-
+#Sets up bar graph with x and y axis
+df.plot.bar(x = "Language", y = "Percent")
+#makes sure all ten languages are printing out
+plt.xticks(range(0, 10), ['English', 'Mandarin Chinese', 'Hindi', 'Spanish', 'French', 'Arabic', 'Bengali', 'Russian', 'Portuguese', 'Urdu'])
+#prints out final graph
 plt.show()
