8ce392ac3d1adfb77879cb278e801682b3847b3e
Veronica Galanter <vgalanter2025@kehillahstudent.org>
2023-05-15 11:01:34 -0700

    Major Languages Spoken by Population Proportion

--- graph.py
+++ graph.py
@@ -3,27 +3,27 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 
-#gets the website and table information
+# #gets the website and table information
 tables = pd.read_html(
-    "https://en.wikipedia.org/wiki/List_of_languages_by_total_number_of_speakers",
-    match = "Most-spoken languages, CIA, 2022"
+     "https://en.wikipedia.org/wiki/List_of_languages_by_total_number_of_speakers",
+     match = "Most-spoken languages, CIA, 2022"
 
 )
 
 table = tables[0]
-#sets up table
+# #sets up table
 df = table[["Language", "Percentage of world population (2022)"]]
-#prints out the percent info
+# #prints out the percent info
 print (df.info())
-#makes percent recognized as a numerical value for printing
+# #makes percent recognized as a numerical value for printing
 df["Percent"]=df["Percentage of world population (2022)"].str.extract('(\d+)').astype(int)
-#prints out percent as numerical value
+# #prints out percent as numerical value
 print (df.info())
-#Sets up bar graph with x and y axis
+# #Sets up bar graph with x and y axis
 df.plot.bar(x = "Language", y = "Percent")
-#makes sure all ten languages are printing out
+# #makes sure all ten languages are printing out
 plt.xticks(range(0, 10), ['English', 'Mandarin Chinese', 'Hindi', 'Spanish', 'French', 'Arabic', 'Bengali', 'Russian', 'Portuguese', 'Urdu'])
-#print title
+# #print title
 plt.title ('Major Languages Spoken by Population Proportion')
-#prints out final graph
+# #prints out final graph
 plt.show()
